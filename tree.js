51ae43d34143b6f1ae7d5622d2103fff6f999b68
Vinogradov Kirill <vikian050194@gmail.com>
2017-08-03 17:55:06 +0300

    Make head field private;

--- tree.js
+++ tree.js
@@ -1,55 +1,32 @@
 function Tree() {
-    this.head = undefined;
-}
+    var head = undefined;
 
-function Node(value, left, right) {
-    this.value = value;
-    this.Left = left;
-    this.Right = right;
-}
+    this.AddNode = function (value) {
+        var node = new Node(value);
 
-Tree.prototype.Delete = function (value) {
-    var observableNode = new Node();
-    observableNode = this.head;
-    if (observableNode.value === value) {
-        if (observableNode.Left === undefined) {
-            if (observableNode.Right === undefined) {
-                this.head = undefined;
-            }
-            else {
-                this.head = observableNode.Right;
-            }
+        if (head === undefined) {
+            head = node;
         }
         else {
-            if (observableNode.Right === undefined) {
-                this.head = observableNode.Left;
-                this.AddNode(observableNode.Right);
-            }
-
-        }
-
-    }
-    else {
-        for (; ;) {
-            if (observableNode.value === value) {
-                //Сюда вывод
-                break;
-            }
-            else {
-                if (observableNode.value < value) {
+            var flag = true;
+            var observableNode = head;
+            while (flag) {
+                if (observableNode.value <= node.value) {
                     if (observableNode.Left === undefined) {
-                        break;
+                        observableNode.Left = node;
+                        flag = false;
                     }
                     else {
-                        //Cюда переход влево
+                        observableNode = observableNode.Left;
                     }
                 }
                 else {
                     if (observableNode.Right === undefined) {
-                        break;
+                        observableNode.Right = node;
+                        flag = false;
                     }
                     else {
-                        //Cюда переход вправо
+                        observableNode = observableNode.Right;
                     }
                 }
 
@@ -57,69 +34,94 @@ Tree.prototype.Delete = function (value) {
 
         }
     }
-}
 
-var Flag = false;
-Tree.prototype.Search = function (value) {
-    var observableNode = new Node();
-    observableNode = this.head;
-    var flag = true;
-    while (flag) {
+    function Node(value, left, right) {
+        this.value = value;
+        this.Left = left;
+        this.Right = right;
+    }
+
+    Tree.prototype.Delete = function (value) {
+        var observableNode = new Node();
+        observableNode = head;
         if (observableNode.value === value) {
-            flag = false;
-            Flag = true;
+            if (observableNode.Left === undefined) {
+                if (observableNode.Right === undefined) {
+                    head = undefined;
+                }
+                else {
+                    head = observableNode.Right;
+                }
+            }
+            else {
+                if (observableNode.Right === undefined) {
+                    head = observableNode.Left;
+                    this.AddNode(observableNode.Right);
+                }
+
+            }
+
         }
         else {
-            if (observableNode.value <= value) {
-                if (observableNode.Left === undefined) {
-                    flag = false;
-
+            for (; ;) {
+                if (observableNode.value === value) {
+                    //Сюда вывод
+                    break;
                 }
                 else {
-                    observableNode = observableNode.Left;
-                }
-            }
-            else {
-                if (observableNode.Right === undefined) {
-                    flag = false;
+                    if (observableNode.value < value) {
+                        if (observableNode.Left === undefined) {
+                            break;
+                        }
+                        else {
+                            //Cюда переход влево
+                        }
+                    }
+                    else {
+                        if (observableNode.Right === undefined) {
+                            break;
+                        }
+                        else {
+                            //Cюда переход вправо
+                        }
+                    }
 
                 }
-                else {
-                    observableNode = observableNode.Right;
-                }
+
             }
         }
     }
-}
 
-Tree.prototype.AddNode = function (node) {
-    if (this.head === undefined) {
-        this.head = node;
-    }
-    else {
+    var Flag = false;
+    Tree.prototype.Search = function (value) {
+        var observableNode = new Node();
+        observableNode = head;
         var flag = true;
-        var observableNode = this.head;
         while (flag) {
-            if (observableNode.value <= node.value) {
-                if (observableNode.Left === undefined) {
-                    observableNode.Left = node;
-                    flag = false;
-                }
-                else {
-                    observableNode = observableNode.Left;
-                }
+            if (observableNode.value === value) {
+                flag = false;
+                Flag = true;
             }
             else {
-                if (observableNode.Right === undefined) {
-                    observableNode.Right = node;
-                    flag = false;
+                if (observableNode.value <= value) {
+                    if (observableNode.Left === undefined) {
+                        flag = false;
+
+                    }
+                    else {
+                        observableNode = observableNode.Left;
+                    }
                 }
                 else {
-                    observableNode = observableNode.Right;
+                    if (observableNode.Right === undefined) {
+                        flag = false;
+
+                    }
+                    else {
+                        observableNode = observableNode.Right;
+                    }
                 }
             }
-
         }
-
     }
 }
